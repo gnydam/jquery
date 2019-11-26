1065916e39c18ea7b7d898736b09954211e5922d
gnydam <33816988+gnydam@users.noreply.github.com>
2019-11-26 16:59:12 -0500

    Add files via upload

---
+++ jquery/challenge1.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('#subscribe').click(function(){
+    $('#emailField').toggle();
+  })
+})
+
+
+// if ($('#subscribe').is(':checked')){
+//   $('#emailField').toggle();
+// javascript:
+// subscribe.onchange = function() {
+//   // console.log("I have been clicked!");
+//   var x = document.querySelector("#emailField");
+//   if (this.checked){
+//     console.log("selected");
+//     x.style.display = "block";
+//   } else {
+//     console.log("Not Selected");
+//     x.style.display = "none";
+//   }
+// }
